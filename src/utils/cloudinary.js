c8e33411e9ea2f970243693b78616aafcd18df97
santoshkakkera <kakkera.santosh@gmail.com>
2025-09-19 16:54:13 -0700

    fixed filename error

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,23 +1,24 @@
-class ApiError extends Error {
-  constructor(
-    statusCode,
-    message = "Something went wrong",
-    errors = [],
-    stack = []
-  ){
-    super(message)
-    this.statusCode = statusCode
-    this.data = null
-    this.message = message
-    this.success = false;
-    this.errors = errors
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY
+});
 
-    if(stack){
-      this.stack = stack
-    } else{
-      Error.captureStackTrace(this, this.constructor)
-    }
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return "Couldnot Find the localFilePath"
+    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    })
+    console.log("File has been uploaded to Cloudinary!!", response.url);
+    return response
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    return null;
   }
 }
 
-export {ApiError}
+export {uploadOnCloudinary}
